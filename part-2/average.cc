05d25c244780788d943aa0578221891457f56dcd
roas23 <143058305+roas23@users.noreply.github.com>
2023-10-31 16:41:22 +0000

    Able to find average

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  if (arguments.size() <= 1) {
+  if (arguments.size() < 2) {
     std::cout << "error: you must supply at least one number\n";
     return 1;
   }
